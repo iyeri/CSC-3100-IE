9e0e2a512c4f6d43ac5029efe82378c2da44745f
iyeri <gavenbui@gmail.com>
2026-09-09 15:44:03 -0700

    Completed step 7 of IE2. Added to backend.js, implementing functionality for deleting users through DELETE request and functionality for searching for users given a name and job through the /users GET endpoint.

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -31,31 +31,35 @@ const users = {
   ],
 };
 
+
 const app = express();
 const port = 8000;
 
 app.use(express.json());
 
-const findUserByName = (name) => {
-  return users["users_list"].filter((user) => user["name"] === name);
-};
-
-const findUserById = (id) => {
-  return users["users_list"].find((user) => user["id"] === id);
-};
-
-const addUser = (user) => {
-  users["users_list"].push(user);
-  return user;
-};
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
+
+const findUserByName = (name) => {
+  return users["users_list"].filter((user) => user["name"] === name);
+};
+
+const findUserByNameAndJob = (name, job) => {
+  return users["users_list"].filter((user) => (user["name"] === name && user["job"] === job));
+};
+
 app.get("/users", (req, res) => {
   const name = req.query.name;
-  if (name != undefined) {
+  const job = req.query.job;
+
+  if (name != undefined && job != undefined) {
+    let result = findUserByNameAndJob(name, job);
+    result = { users_list: result };
+    res.send(result);
+  } else if (name != undefined) {
     let result = findUserByName(name);
     result = { users_list: result };
     res.send(result);
@@ -63,6 +67,11 @@ app.get("/users", (req, res) => {
     res.send(users);
   }
 });
+
+
+const findUserById = (id) => {
+  return users["users_list"].find((user) => user["id"] === id);
+};
 
 app.get("/users/:id", (req, res) => {
   const id = req.params["id"] // or req.params.id
@@ -74,12 +83,42 @@ app.get("/users/:id", (req, res) => {
   }
 });
 
+
+const addUser = (user) => {
+  users["users_list"].push(user);
+  return user;
+};
+
 // Ensure that the POST request is json! (the "Content-Type" header should be set to "application/json")
 app.post("/users", (req, res) => {
   const userToAdd = req.body;
   addUser(userToAdd);
   res.send();
 });
+
+
+const deleteUserById = (id) => {
+  const userToDelete = findUserById(id);
+  if (userToDelete === undefined) {
+    return undefined;
+  }
+
+  const updatedUsers = users["users_list"].filter((user) => user["id"] !== id);
+  users["users_list"] = updatedUsers;
+  
+  return userToDelete;
+}
+
+app.delete("/users/:id", (req, res) => {
+  const id = req.params.id // or req.params["id"]
+  let result = deleteUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
